b37a4f399e2ab97c60ce52dadfc66ac3bcb0f631
Jaehong, Oh <ojh6t3k@gmail.com>
2016-09-07 09:40:01 +0900

    Day #1 수업 -유니티 프로젝트 만들기 -MyComponent 연습

---
+++ Assets/Lecture/Scripts/Editor/MyComponentEditor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+
+[CustomEditor(typeof(MyComponent))]
+public class MyComponentEditor : Editor
+{
+	void OnEnable()
+	{
+	}
+
+	public override void OnInspectorGUI()
+	{
+		MyComponent myComponent = (MyComponent)target;
+
+		myComponent.intVariable = EditorGUILayout.IntField("Int Variable", myComponent.intVariable);
+		myComponent.floatVariable = EditorGUILayout.Slider("Float Variable", myComponent.floatVariable, 0f, 100f);
+		myComponent.IntVar = EditorGUILayout.IntField("Int Var", myComponent.IntVar);
+		if(GUILayout.Button("Do something") == true)
+		{
+			myComponent.DoSomething();
+		}
+	}
+}
---
+++ Assets/Lecture/Scripts/MyComponent.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyComponent : MonoBehaviour
+{
+	public int intVariable;
+	public float floatVariable;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public int IntVar
+	{
+		get
+		{
+			return intVariable;
+		}
+		set
+		{
+			intVariable = value;
+		}
+	}
+
+	public void DoSomething()
+	{
+		intVariable++;
+	}
+}
