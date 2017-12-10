f515c22f742c1b1305438014f8bd47e96c356723
Jaehong, Oh <ojh6t3k@gmail.com>
2017-12-10 19:05:04 +0900

    마지막 업데이트

---
+++ Assets/TutorProject/Scenes/Examples/Network/NetworkObject.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+public class NetworkObject : NetworkBehaviour
+{
+	[SyncVar]
+	public int health = 100;
+
+	private float time = 0;
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		time += Time.deltaTime;
+		if(time > 1f)
+		{
+			if(isServer == true)
+				health--;
+			
+			time = 0;
+			Debug.Log(health);
+		}
+	}
+
+	[ClientRpc]
+	void Rpc_DiePlayer()
+	{
+	}
+
+	[Command]
+	public void Cmd_RefilllHealth()
+	{
+		health = 100;
+	}
+}
